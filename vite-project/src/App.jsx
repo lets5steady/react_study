460518b126c42d33307efa01db4b7c0e3b5ae7ca
lets5steady <lets.05.steady@gmail.com>
2026-05-08 15:57:13 +0900

    chapter4_P181まで

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import FormCheck from './components/FormCheck'
+import FormCheckMuiti from './components/FormCheckMuiti'
+import FormFile from './components/FormFile'
+import FormRadio from './components/FormRadio'
+import FormSelect from './components/FormSelect'
+import FormTextarea from './components/FormTextarea'
 
 function App() {
 
   return (
     <>
       <h1>2026年5月8日</h1>
+      <FormTextarea />
+      <FormSelect />
+      <FormRadio />
+      <FormCheck />
+      <FormCheckMuiti />
+      <FormFile />
     </>
   )
 }
---
+++ vite-project/src/components/FormRadio.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+export default function FormRadio() {
+
+    const [form, setForm] = useState({
+        os: 'windows'    
+    });
+
+    const handleForm = e => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const show = () => {
+        console.log(`使用OS：${form.os}`);        
+    }
+
+    return(
+        <form>
+            <fieldset>
+                <legend>使用OS：</legend>
+                
+                <label htmlFor="os_win">Windows</label>
+                <input type="radio" name="os" id="os_win"
+                    value="windows" checked = {form.os === 'windows'}
+                    onChange={handleForm}/><br />
+
+                <label htmlFor="os_mac">macOS</label>
+                <input type="radio" name="os" id="os_mac"
+                    value="mac" checked = {form.os === 'mac'}
+                    onChange={handleForm}/><br />
+
+                <label htmlFor="os_lin">Linux</label>
+                <input type="radio" name="os" id="os_lin"
+                    value="linux" checked = {form.os === 'linux'}
+                    onChange={handleForm}/>
+            </fieldset>
+
+            <button type="button" onClick={show}>送信</button>
+        </form>
+    );
+}
