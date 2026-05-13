dc55c0ec0f67400ca9b75e7593885b892e22d77f
lets5steady <lets.05.steady@gmail.com>
2026-05-13 12:49:42 +0900

    教科書Chapter5終了。

---
+++ vite-project/src/components/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+import styles from './Loading.module.css'
+
+export default function Loading() {
+    return(
+        <>
+            <p>読み込み中</p>
+        </>
+    );
+}
---
+++ vite-project/src/components/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import styles from './Profile.module.css'
+
+export default function Profile() {
+  return (
+    <section className={styles.box}>
+      <h2>プロフィール</h2>
+      <p>ここにユーザーのプロフィール情報を表示します。</p>
+    </section>
+  );
+}
+
