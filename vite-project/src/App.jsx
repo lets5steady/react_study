53e9a6955433837ad02ff8a8adb473b65fe812d1
lets5steady <lets.05.steady@gmail.com>
2026-05-19 12:33:41 +0900

    Chapter7_P416まで

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import HookContext from './components/HookContext/HookContext'
 
 function App() {
 
   return (
     <>
       <h1>Vite+React初期状態</h1>
+      <HookContext />
     </>
   )
 }
